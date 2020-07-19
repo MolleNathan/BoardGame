bf3afc3818c179e91c55ad5d0ecc363034480baf
Nathan <nathanmolle25@gmail.com>
2020-07-19 20:09:34 +0200

    first commit

---
+++ src/test/java/com/example/nathan/molle/BoardGame/BoardGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nathan.molle.BoardGame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
